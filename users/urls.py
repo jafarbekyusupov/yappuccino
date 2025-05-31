8eed24fcb434ffe492070054788d79d9b40ed687
jafarbekyusupov <github.jafarbekyusupov@gmail.com>
2025-05-31 16:43:36 +0500

    added some test views to figure out wth is wrong w default_storage stg

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('export-data/', views.export_user_data, name='export-data'),
     path('delete-account/', views.delete_account_confirm, name='delete-account-confirm'),
     path('test-storage/', views.test_storage, name='test-storage'),
-    path('debug-storage/', views.debug_storage_comprehensive, name='debug-storage'),
+    path('debug-storage/', views.debug_storage, name='debug-storage'),
     path('test-storage-reload/', views.test_storage_reload, name='test-storage-reload'),
 ]
