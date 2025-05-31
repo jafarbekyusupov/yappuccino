e4e8067c017f011ab6ce9a335bbc99bfa4edf259
jafarbekyusupov <github.jafarbekyusupov@gmail.com>
2025-05-31 19:54:14 +0500

    disable ACL
    fix urls in base.html

--- blogpost/production.py
+++ blogpost/production.py
@@ -42,7 +42,7 @@ if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
     print("===================================== AWS S3 CONFIG =====================================")
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_FILE_OVERWRITE = True
 
